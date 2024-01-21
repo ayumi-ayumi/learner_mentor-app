5c13b32618399734ce1812c2d376b8ec50b5681d
Ayumi <ayuayuayuayus@gmail.com>
2024-01-21 22:37:35 +0100

    Added an pop up InfoWindow

--- src/components/ShowMap.jsx
+++ src/components/ShowMap.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/ShowMap.scss";
 import {
   APIProvider,
@@ -6,69 +6,149 @@ import {
   AdvancedMarker,
   Pin,
   InfoWindow,
-  Marker
+  Marker,
 } from "@vis.gl/react-google-maps";
+// import { v4 as uuidv4 } from "uuid";
 
 const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 export default function ShowMap() {
-  const position = { lat: 52.52, lng: 13.41 }; //Berlin
-  const [open, setOpen] = React.useState(false);
+  // const position = { lat: 52.52, lng: 13.41 }; //Berlin
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState({});
+
+  // const onSelect = item => {
+  //   setSelected(item);
+  // }
+
+  const [activeMarker, setActiveMarker] = React.useState(null);
+  const handleActiveMarker = (marker) => {
+    if (marker === activeMarker) {
+      return;
+    }
+    setActiveMarker(marker); //id:3
+  };
+
+  const [selectPlace, setSelectPlace] = useState(null);
+
+  const onMarkerClick = (marker) => {
+    setSelectPlace(marker);
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setSelectPlace(null);
+  };
+
   const markers = [
     {
       id: 1,
-      name: "Chicago, Illinois",
-      position: { lat: 41.881832, lng: -87.623177 }
+      name: "Treptower Park",
+      position: { lat: 52.488449, lng: 13.469631 },
     },
     {
       id: 2,
-      name: "Denver, Colorado",
-      position: { lat: 39.739235, lng: -104.99025 }
+      name: "Tempelhofer Feld ",
+      position: { lat: 52.474926, lng: 13.400312 },
     },
     {
       id: 3,
-      name: "Los Angeles, California",
-      position: { lat: 34.052235, lng: -118.243683 }
+      name: "Alexanderplatz",
+      position: { lat: 52.521992, lng: 13.413244 },
     },
     {
       id: 4,
-      name: "New York, New York",
-      position: { lat: 40.712776, lng: -74.005974 }
-    }
+      name: "Tegel airport",
+      position: { lat: 52.554803, lng: 13.28903 },
+    },
   ];
 
   return (
     <APIProvider apiKey={API_KEY}>
       <div id="map">
-        {/* <div style={{ height: "80vh", width: "90%" }}> */}
         <Map
-          zoom={5}
-          center={{lat: 41.881832, lng: -87.623177}}
+          zoom={11}
+          center={{ lat: 52.52, lng: 13.41 }}
           gestureHandling={"greedy"}
-          disableDefaultUI={false} //trueにすると、ズームのアイコンなどが全て非表示になる
+          disableDefaultUI={false} //trueにすると、ズームのボタンなどが全て非表示になる
           mapId={import.meta.env.VITE_GOOGLE_MAPS_ID} //To use a marker, map ID is needed
+          onClick={() => setActiveMarker(null)}
         >
-          {/* <Marker position={position} onClick={() => setOpen(true)} /> */}
-          {markers.map(( {id, position} ) => (
+          {markers.map((marker) => (
+            //{markers.map(({ id, name, position }) => (
             <Marker
-              key={id}
-              position={position}
-              onClick={() => handleActiveMarker(id)}
-            />))}
-          {/* <AdvancedMarker position={position} onClick={() => setOpen(true)}> //AdvancedMarkerだとアイコンをカスタマイズできる
-            <Pin
-              background={"green"}
-              borderColor={"pink"}
-              glyphColor={"yellow"}
-            ></Pin>
-          </AdvancedMarker> */}
-          {open && (
-            <InfoWindow position={position} onCloseClick={() => setOpen(false)} >
-              <p style={{ backgroundColor: "yellow" }}>I'm in Berlin!</p>
+              key={crypto.randomUUID()}
+              position={marker.position}
+              // onClick={() => console.log(id)}
+              // onClick={() => setOpen(true)}
+              onClick={() => setSelected(marker)}
+              // onClick={() => handleActiveMarker(id)}
+
+              // onClick={() => handleActiveMarker(id)}
+              // onClick={() => onMarkerClick(marker)}
+            />
+          ))}
+              {selected &&  (
+                <InfoWindow
+                  position={selected.position}
+                  onCloseClick={() => setSelected(null)}
+                >
+                  <p style={{ backgroundColor: "yellow" }}>
+                    I'm in {selected.name}!
+                  </p>
+                </InfoWindow>
+              ) }
+
+          {/* {open && (
+          //{selectPlace && (
+            <InfoWindow
+              position={selectPlace.position}
+              // onCloseClick={onClose}
+              onCloseClick={() => onClose()}
+              onCloseClick={() => setOpen(false)}
+            >
+              <p style={{ backgroundColor: "yellow" }}>
+                I'm in {selectPlace.name}!
+              </p>
             </InfoWindow>
-          )}
+          )} */}
         </Map>
       </div>
     </APIProvider>
   );
+
+  // return (
+  //   <APIProvider apiKey={API_KEY}>
+  //     <div id="map">
+  //       {/* <div style={{ height: "80vh", width: "90%" }}> */}
+  //       <Map
+  //         zoom={11}
+  //         center={{lat: 52.52, lng: 13.41}}
+  //         gestureHandling={"greedy"}
+  //         disableDefaultUI={false} //trueにすると、ズームのボタンなどが全て非表示になる
+  //         mapId={import.meta.env.VITE_GOOGLE_MAPS_ID} //To use a marker, map ID is needed
+  //       >
+  //         <Marker onClick={() => setOpen(true)} />
+  //         {markers.map(( {id, position} ) => (
+  //           <Marker
+  //             key={id}
+  //             position={position}
+  //             // onClick={() => handleActiveMarker(id)}
+  //           />))}
+  //         {/* <AdvancedMarker position={position} onClick={() => setOpen(true)}> //AdvancedMarkerだとアイコンをカスタマイズできる
+  //           <Pin
+  //             background={"green"}
+  //             borderColor={"pink"}
+  //             glyphColor={"yellow"}
+  //           ></Pin>
+  //         </AdvancedMarker> */}
+  //         {open && (
+  //           <InfoWindow position={position} onCloseClick={() => setOpen(false)} >
+  //             <p style={{ backgroundColor: "yellow" }}>I'm in Berlin!</p>
+  //           </InfoWindow>
+  //         )}
+  //       </Map>
+  //     </div>
+  //   </APIProvider>
+  // );
 }
