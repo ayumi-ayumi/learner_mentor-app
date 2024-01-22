121c9dff609de6674416815b4fb8860712e60724
Ayumi <ayuayuayuayus@gmail.com>
2024-01-22 09:53:13 +0100

    Added React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import ShowMap from "./components/ShowMap";
+import Test from "./components/Test";
+
 
 export default function App() {
   return (
-    <div>
-      <ShowMap />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ShowMap />} />
+      <Route path="/test" element={<Test />} />
+    </Routes>
+    </BrowserRouter>
+
   );
 }
