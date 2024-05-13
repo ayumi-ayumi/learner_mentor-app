01b9550e75cbe5ae42fc3614c1fc6dcafd22ddc0
Ayumi <ayuayuayuayus@gmail.com>
2024-05-13 23:20:40 +0200

    Editted the marker card

--- src/components/PlaceMarker.jsx
+++ src/components/PlaceMarker.jsx
@@ -42,7 +42,7 @@ export default function PlaceMarker({ user, isOpen, setMarkerPlaceId }) {
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-  
+
   return (
     <>
       <AdvancedMarker
@@ -99,26 +99,26 @@ export default function PlaceMarker({ user, isOpen, setMarkerPlaceId }) {
                 title="Shrimp and Chorizo Paella"
                 subheader="September 14, 2016"
               />
-              <CardMedia
+              {/* <CardMedia
                 component="img"
                 height="194"
                 image="/static/images/cards/paella.jpg"
                 alt="Paella dish"
-              />
+              /> */}
               <CardContent>
-                <Typography variant="body2" color="text.secondary">
+                {/* <Typography variant="body2" color="text.secondary">
                   This impressive paella is a perfect party dish and a fun meal
                   to cook together with your guests. Add 1 cup of frozen peas
                   along with the mussels, if you like.
-                </Typography>
+                </Typography> */}
               </CardContent>
               <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
+                {/* <IconButton aria-label="add to favorites">
                   <FavoriteIcon />
                 </IconButton>
                 <IconButton aria-label="share">
                   <ShareIcon />
-                </IconButton>
+                </IconButton> */}
                 <ExpandMore
                   expand={expanded}
                   onClick={handleExpandClick}
