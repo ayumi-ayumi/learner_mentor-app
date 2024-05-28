493292f9ad56ae35b6282a55c085cc696a01ab51
Ayumi <ayuayuayuayus@gmail.com>
2024-05-28 16:38:37 +0200

    Completed using useFormContext on FormInputDropdown component

--- src/form-components/FormInputDropdown.tsx
+++ src/form-components/FormInputDropdown.tsx
@@ -19,9 +19,18 @@ export const FormInputDropdown = ({
   // }, [name, setValue, languages]);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const handleChange = (event: any) => {
-    setLanguages(event.target.value);
-    console.log(123)
+  // const handleChange = (event: any) => {
+  //   setLanguages(event.target.value);
+  //   console.log(event)
+  // };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleChange = (event: { target: { value: any; }; }) => {
+    const { target: { value }, } = event;
+    setLanguages(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
   };
 
   const generateSingleOptions = () => {
@@ -40,37 +49,19 @@ export const FormInputDropdown = ({
     <Controller
       control={control}
       name={name}
+      rules={{
+        required: {
+          value: true,
+          message:
+            'Choose your spoken languages',
+        },
+      }}
       render={({ field: { value, onChange }, fieldState: { error } }) => (
         <FormControl error={!!error} >
           <FormLabel>{label}</FormLabel>
-          {/* <FormGroup row={true}>
-          {options?.map((option) => (
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={value.includes(option.value)}
-                  onChange={() => {
-                    if (value.includes(option.value)) {
-                      onChange(
-                        (value as string[]).filter(
-                          (item) => item !== option.value
-                        )
-                      );
-                    } else {
-                      onChange([...value, option.value]);
-                    }
-                  }}
-                  key={option.value}
-                />
-              }
-              label={option.label}
-              key={option.value}
-            />
-          ))}
-        </FormGroup> */}
           <Select
             onChange={handleChange}
-            value={languages}
+            value={value}
             label="Languages"
             multiple
             input={<OutlinedInput label="Tag" />}
@@ -79,14 +70,12 @@ export const FormInputDropdown = ({
             <FormGroup>
               {options?.map((option) => (
                 <FormControlLabel
+                  label={option.label}
+                  key={option.value}
                   control={
-                    <MenuItem key={option.value} >
-
-                      <Checkbox
-                        checked={value.includes(option.value)}
+                    <MenuItem key={option.value} value={option.value}>
+                      <Checkbox checked={value.includes(option.value)}
                         onChange={() => {
-
-
                           if (value.includes(option.value)) {
                             onChange(
                               (value as string[]).filter(
@@ -95,16 +84,10 @@ export const FormInputDropdown = ({
                             );
                           } else {
                             onChange([...value, option.value]);
-
                           }
-                        }}
-                        key={option.value}
-                      />
+                        }} />
                     </MenuItem>
-
                   }
-                  label={option.label}
-                  key={option.value}
                 />
               ))}
             </FormGroup>
@@ -115,80 +98,3 @@ export const FormInputDropdown = ({
     ></Controller>
   )
 }
-//   return (
-//     <FormControl size={"small"}>
-//       <InputLabel>{label}</InputLabel>
-//       <Controller
-//         render={() => (
-//           <Select
-//             onChange={handleChange}
-//             value={languages}
-//             label="Languages"
-//             multiple
-//             input={<OutlinedInput label="Tag" />}
-//             renderValue={(selected) => selected.join(', ')}
-//           >
-//             {generateSingleOptions()}
-//           </Select>
-//         )}
-//         control={control}
-//         name={name}
-//       />
-//     </FormControl>
-//   );
-// };
-
-
-
-// export const FormInputDropdown = ({
-//   name,
-//   control,
-//   label,
-//   options,
-//   setValue,
-// }: FormInputProps) => {
-//   const [languages, setLanguages] = useState([]);
-
-//   useEffect(() => {
-//     if (languages) setValue(name, languages);
-//   }, [name, setValue, languages]);
-
-//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   const handleChange = (event: any) => {
-//     setLanguages(event.target.value);
-//   };
-
-//   const generateSingleOptions = () => {
-//     if (typeof options !== "undefined")
-//       return options.map((option) => {
-//         return (
-//           <MenuItem key={option.value} value={option.value}>
-//             <Checkbox checked={languages.indexOf(option.value as never) > -1} />
-//             <ListItemText primary={option.value} />
-//           </MenuItem>
-//         );
-//       });
-//   };
-
-//   return (
-//     <FormControl size={"small"}>
-//       <InputLabel>{label}</InputLabel>
-//       <Controller
-//         render={() => (
-//           <Select
-//             onChange={handleChange}
-//             value={languages}
-//             label="Languages"
-//             multiple
-//             input={<OutlinedInput label="Tag" />}
-//             renderValue={(selected) => selected.join(', ')}
-//           >
-//             {generateSingleOptions()}
-//           </Select>
-//         )}
-//         control={control}
-//         name={name}
-//       />
-//     </FormControl>
-//   );
-// };
