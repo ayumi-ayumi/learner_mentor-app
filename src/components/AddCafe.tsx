f4b6681dd5dbb4c98009527c3b49eff8ff522802
Ayumi <ayuayuayuayus@gmail.com>
2024-06-08 23:45:42 +0200

    Created AddCafe.tsx

---
+++ src/components/AddCafe.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddCafe() {
+  return (
+    <div>AddCafe</div>
+  )
+}
--- src/interfaces/interfaces.tsx
+++ src/interfaces/interfaces.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+
 export interface UserProfile {
   uid: string,
   id: number,
