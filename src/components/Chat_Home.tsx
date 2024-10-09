f9d66975d79037b063d8b38780c3c9f91c4b690f
Ayumi <ayuayuayuayus@gmail.com>
2024-10-09 23:42:33 +0200

    Added useLocation on Chat_Home.tsx

--- src/components/Chat_Home.tsx
+++ src/components/Chat_Home.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './Sidebar'
 import Chat from './Chat_new'
+import { useAuth } from '../context/AuthProvider';
+import { useLocation, useParams } from 'react-router-dom';
 
 export default function Chat_Home () {
+  const { users } = useAuth();
+  const location = useLocation()
+  const [uid, setUid]
+    = useState(location.state.uid)
+  const sendTo = users.find((user) => user.uid === uid);
+  console.log(sendTo)
+
   return (
     <div className='home'>
       <div className="container">
