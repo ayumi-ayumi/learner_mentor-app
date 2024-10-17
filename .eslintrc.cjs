5bea9139b52e78a4d009fb7591ecdebfbee72313
Ayumi <ayuayuayuayus@gmail.com>
2024-10-17 02:07:21 +0200

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    "plugin:@typescript-eslint/recommended",
+    'plugin:react/recommended',
+    // 'plugin:react-hooks/recommended',
+    // 'prettier/@typescript-eslint',
+    "prettier"
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    // TypeScript用設定を追加
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  settings: { react: { version: '18.2' } },
+  plugins: ["@typescript-eslint", 'react'],
+  parser: "@typescript-eslint/parser",
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    'react/prop-types': 'off',
+  },
+  "overrides": [
+    {
+        "env": {
+            "node": true
+        },
+        "files": [
+            ".eslintrc.{js,cjs}"
+        ],
+        "parserOptions": {
+            "sourceType": "script"
+        }
+    }
+],
+}
